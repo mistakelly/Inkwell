ab19148bd84a2ea291cf9de202300169106a44a6
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-08-26 10:09:59 +0000

    Refactored signup Logic, with regards to keeping the codebase Dry. - Removed setTimeout method from the fetch method. - Created different functions for handling user registeration logic. eg .. (validateUsername, validateEmail, registerUser) - Seprated event listners from core function functionality and added it as callback. - Check the existence of tags before attaching an eventlistner in order to avoid attaching event listeners to null datatype.

--- flaskblog/static/js/signup.js
+++ flaskblog/static/js/signup.js
@@ -1,10 +1,9 @@
-// Flag to track the validation state
+// FLAG FOR TRACKING USERNAME AND EMAIL VALIDATION.
 let isUsernameValid = false;
 let isEmailValid = false;
 
 // Selectors
 const body = document.body;
-console.log(body);
 
 const emailInput = document.getElementById("email-input");
 const usernameInput = document.getElementById("username-input");
@@ -12,8 +11,9 @@ const passwordInput = document.getElementById("password-input");
 const form = document.querySelector(".form");
 const feedback = document.querySelector(".feedback--success");
 const spinner = document.querySelector(".spinner-container");
+const submitBtn = document.querySelector(".signup-btn");
 
-// Helper function
+// HELPER FUNCTIONS
 const setLabel = (labelElement, text, color) => {
   labelElement.textContent = text;
   labelElement.style.color = color;
@@ -25,77 +25,69 @@ const clearFormInput = function () {
   passwordInput.value = "";
 };
 
-// Username Validation Function
+// VALIDATE USERNAME FUNCTION.
 const validateUsername = function () {
   const usernameRegex = /^[a-z][a-z0-9]{0,9}$/;
   const usernameLabel = document.querySelector(".username-label");
 
-  usernameInput.addEventListener("input", (e) => {
-    const usernameValue = e.target.value;
+  const usernameValue = e.target.value;
 
-    if (usernameValue.length === 0) {
-      setLabel(usernameLabel, "Username is required", "red");
-      isUsernameValid = false;
-    } else if (!/^[a-z]/.test(usernameValue)) {
-      setLabel(usernameLabel, "Username must start with a letter", "red");
-      isUsernameValid = false;
-    } else if (!usernameRegex.test(usernameValue)) {
-      setLabel(
-        usernameLabel,
-        "Username can contain letters and numbers, up to 10 characters",
-        "red"
-      );
-      isUsernameValid = false;
-    } else {
-      setLabel(usernameLabel, "Username", "black");
-      isUsernameValid = true;
-    }
-  });
+  if (usernameValue.length === 0) {
+    setLabel(usernameLabel, "Username is required", "red");
+    isUsernameValid = false;
+  } else if (!/^[a-z]/.test(usernameValue)) {
+    setLabel(usernameLabel, "Username must start with a letter", "red");
+    isUsernameValid = false;
+  } else if (!usernameRegex.test(usernameValue)) {
+    setLabel(
+      usernameLabel,
+      "Username can contain letters and numbers, up to 10 characters",
+      "red"
+    );
+    isUsernameValid = false;
+  } else {
+    setLabel(usernameLabel, "Username", "black");
+    isUsernameValid = true;
+  }
 };
 
-// Email validation function
+// VALIDATE EMAIL FUNCTION.
 const validateEmail = function () {
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   const emailLabel = document.querySelector(".email-label");
 
-  emailInput.addEventListener("input", (e) => {
-    const emailValue = e.target.value;
+  const emailValue = e.target.value;
 
-    if (emailValue.length === 0) {
-      setLabel(emailLabel, "Email is required", "red");
-      isEmailValid = false;
-    } else if (!/@/.test(emailValue)) {
-      setLabel(emailLabel, "Email must contain an '@' symbol", "red");
-      isEmailValid = false;
-    } else if (!/\.[a-zA-Z]{2,}$/.test(emailValue)) {
-      setLabel(emailLabel, "Email domain must be valid, e.g., '.com'", "red");
-      isEmailValid = false;
-    } else if (!emailRegex.test(emailValue)) {
-      setLabel(emailLabel, "Email format is invalid", "red");
-      isEmailValid = false;
-    } else {
-      setLabel(emailLabel, "Email", "black");
-      isEmailValid = true;
-    }
-  });
+  if (emailValue.length === 0) {
+    setLabel(emailLabel, "Email is required", "red");
+    isEmailValid = false;
+  } else if (!/@/.test(emailValue)) {
+    setLabel(emailLabel, "Email must contain an '@' symbol", "red");
+    isEmailValid = false;
+  } else if (!/\.[a-zA-Z]{2,}$/.test(emailValue)) {
+    setLabel(emailLabel, "Email domain must be valid, e.g., '.com'", "red");
+    isEmailValid = false;
+  } else if (!emailRegex.test(emailValue)) {
+    setLabel(emailLabel, "Email format is invalid", "red");
+    isEmailValid = false;
+  } else {
+    setLabel(emailLabel, "Email", "black");
+    isEmailValid = true;
+  }
 };
 
-const submitBtn = document.querySelector(".signup-btn");
-
-form.addEventListener("submit", (e) => {
+// USER REGISTRATION.
+const registerUser = function (e) {
   e.preventDefault();
 
   // Input Values
   const username = usernameInput.value;
   const email = emailInput.value;
   const password = passwordInput.value;
+  const feedbackSpan = feedback.children[1];
 
-  // Check if both the email and username are valid before allowing submission
   if (isUsernameValid && isEmailValid) {
-    console.log("Form submission allowed");
-
-    // Display the modal and spinner
-    clearFormInput();
+    // Display the modal and spinner immediately
     body.classList.add("modal-active");
     spinner.style.display = "block";
 
@@ -112,19 +104,46 @@ form.addEventListener("submit", (e) => {
       }),
     })
       .then((response) => response.json())
-      .then((data) => {
-        // Wait for 1 second before showing the feedback message
-        setTimeout(() => {
-          body.classList.remove("modal-active");
-          spinner.style.display = "none";
-          feedback.children[1].textContent = `${data.welcome_message} ${data.username},!`;
-          feedback.style.visibility = "visible";
-        }, 1000); // Hide modal after 1 second.
-      })
-      .catch((error) => (feedback.children[1].textContent = error));
-  }
-});
+      .then((result) => {
+        // Hide the spinner
+        body.classList.remove("modal-active");
+        spinner.style.display = "none";
 
-// Call the validation functions to set up the event listeners
-validateUsername();
-validateEmail();
+        // Check for errors
+        if (result.errors) {
+          if (result.errors["username_and_email_taken"]) {
+            feedbackSpan.textContent = `${result.errors["username_and_email_taken"]}`;
+          } else if (result.errors["username_taken"]) {
+            feedbackSpan.textContent = result.errors["username_taken"];
+          } else if (result.errors["email_taken"]) {
+            feedbackSpan.textContent = result.errors["email_taken"];
+          }
+
+          feedback.classList.add("feedback--error");
+          feedback.style.visibility = "visible";
+        } else {
+          // Show success feedback
+          clearFormInput();
+          feedback.classList.remove("feedback--error");
+          feedbackSpan.textContent = `${result.welcome_message} ${result.username},!`;
+          feedback.style.visibility = "visible";
+        }
+      })
+      .catch((error) => {
+        // Hide the spinner on error
+        clearFormInput();
+        body.classList.remove("modal-active");
+        spinner.style.display = "none";
+        feedback.classList.add("error");
+        feedbackSpan.textContent =
+          "An unexpected error occurred. Please try again.";
+        feedback.style.visibility = "visible";
+      });
+  }
+};
+// Function calls
+//check for tag availablity before attaching eventlistners to avoid attaching event listners on null datatype.
+if (form) form.addEventListener("submit", registerUser);
+// Call the validation functions to set up the input event listeners
+if (emailInput) emailInput.addEventListener("input", validateEmail);
+if (usernameInput) usernameInput.addEventListener("input", validateUsername);
--- flaskblog/users/routes.py
+++ flaskblog/users/routes.py
@@ -1,7 +1,6 @@
-from flask import Blueprint, redirect, flash, url_for, render_template, session, request, current_app
+from flask import Blueprint, redirect, flash, url_for, render_template, session, request, current_app, jsonify
 from flask_login import current_user, logout_user, login_user, login_required
 from werkzeug.utils import secure_filename
-from flask import jsonify
 from uuid import uuid4
 import os
 from flaskblog.users.forms import (
@@ -27,7 +26,31 @@ def register_page():
     
     #  check if request is Post
     if request.method == "POST":
+        # convert to dictionary
         data = request.get_json()
+
+        # extract credentials
+        username = data.get('username')
+        email = data.get('email')
+
+        # validate user credentials does not exists before attempting to persist to db.
+
+        # declare an empty dictionary for error handling
+        errors = {}
+
+
+        if User.query.filter_by(username=username).first() and User.query.filter_by(email=email).first():
+            errors['username_and_email_taken'] = 'Username already exists and Email already registered, Please choose a different ones'
+        elif User.query.filter_by(username=username).first():
+            errors['username_taken'] = 'Username already exists. Please choose a different one.'
+
+        elif User.query.filter_by(email=email).first():
+            errors['email_taken'] = 'Email already registered. Please use a different email address.'
+
+
+        if errors:
+            print(errors)
+            return jsonify({'errors': errors})
         
         # save user in the database.
         user = User(
@@ -42,3 +65,147 @@ def register_page():
         return jsonify({"welcome_message": 'Thank you for Joining us', 'redirect_url': url_for('users.login_page'), 'username': data.get('username')})
     else:
         return render_template("register.html")
+
+
+# User Login Route
+@users.route("/login", strict_slashes=False, methods=["GET", "POST"])
+def login_page():
+    print('login paguihiuhe')
+    # if current_user.is_authenticated:
+    #     return redirect(url_for("main.index_page"))
+    """
+    Get users email from form email field
+    Check if  email exists in database
+    if email exists:
+        User has account in our database
+        Unhash the password of the user
+        Compare the the unhashed password with the form password.
+        if Match:
+            Login the User in, add remember me field.
+    """
+    # form = Login_Form()
+    if request.method == 'POST':
+        data = request.get_json()
+
+        email = data.get('email')
+        password = data.get('password')
+
+        print('user email', email)
+        print('user password', password)
+        user = db.session.query(User).filter_by(email=email).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+
+            # log the user in using session object from flask
+            login_user(user)
+
+            return jsonify({'status':'success','login_message': f"Success! You're logged in and ready to go, {user.username}", 'redirect_url': url_for('main.index_page')}), 200
+                            
+
+        else:
+            return jsonify({"status": 'failure', 'login_message': 'Invalid username or password'}), 400
+
+
+    return render_template("login.html")
+
+
+@users.route("/account", methods=["GET", "POST"])
+@login_required
+def account_page():
+    form = Update_Account_Form()
+
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+
+        # update profile pic.
+        if form.profile_pic.data:
+            profile_pic_filname = secure_filename(form.profile_pic.data.filename)
+
+            _, file_ext = os.path.splitext(profile_pic_filname)
+
+            random_filename = f"{uuid4()}{file_ext}"
+
+            profile_pic_ = os.path.join(current_app.config["UPLOAD_FOLDER"], random_filename)
+            form.profile_pic.data.save(profile_pic_)
+            current_user.image_file = random_filename
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            flash(f"An error occured {e}")
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    img_file = url_for("static", filename="profile_pics/" + "default.jpg")
+    return render_template("account.html", form=form, img_file=img_file)
+
+
+# logout route
+@users.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("main.index_page"))
+
+
+# Send TOTP for reseting email.
+@users.route("/password/reset/", methods=["GET", "POST"])
+def send_email_link():
+    form = Reset_Request_Form()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            user = db.session.query(User).filter_by(email=form.email.data).first()
+            print(user)
+            if user:
+                # try:
+                send_email(user)
+
+                flash(
+                    "Email sent successfully!, follow link to reset your password",
+                    category="success",
+                )
+                # except Exception as e:
+                # flash(f"Failed to send email: {e}", category="error")
+
+                return redirect(url_for("users.send_email_link"))
+            else:
+                flash("Email does not exist in our system", category="error")
+
+    return render_template("reset_email.html", form=form)
+
+
+# Password Reset
+@users.route("/password/reset/<token>", methods=["GET", "POST"])
+def reset_password(token):
+    form = Reset_Password()
+    user = User.validate_token(token, expire_sec=300)
+
+    if request.method == "POST" and form.validate_on_submit():
+        # user variable would be None if the time set on the link expires.
+        if user is None:
+            flash(
+                "Error: The provided token is either invalid or has expired",
+                category="error",
+            )
+            return redirect(url_for("users.send_email_link"))
+
+        #  hash user password
+        user.password = bcrypt.generate_password_hash(form.password.data).decode(
+            "utf-8"
+        )
+
+        db.session.commit()
+        flash(
+            "Your password has been successfully reset. You can now log in with your new password.",
+            category="success",
+        )
+
+        # redirect user to login page
+        return redirect(url_for("users.login_page"))
+
+    # render reset password template on GET REQUEST.
+    return render_template(
+        "reset_password.html",
+        form=form,
+        legend_title="Reset your password",
+        title="Password reset",
+    )
