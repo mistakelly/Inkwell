811356e8a0110504c2e3c28be7b38af36f1f98b9
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-08-28 21:09:19 +0000

    add application configurations

---
+++ flaskblog/config.py
@@ -0,0 +1,24 @@
+import os
+class Config:
+    SECRET_KEY = os.environ.get('FLASKBLOG_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('FLASKBLOG_DB')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Mail config
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = os.environ.get('EMAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+    UPLOAD_FOLDER = (os.environ.get('FLASKBLOG_PROFILE_PATH'))
+
+
+
+
+check = Config()
+print(check.UPLOAD_FOLDER)
+print(check.MAIL_PASSWORD)
+print(check.MAIL_USERNAME)
+
+
+
