24b74b95c9545aa7bc12bd36415678dc47d2ff16
Kelvin Okoye <kelvinokoye413@gmail.com>
2025-07-14 23:43:25 +0100

    update

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,4 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:
     SECRET_KEY = os.environ.get('FLASKBLOG_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('FLASKBLOG_DB')
